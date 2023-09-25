5ad12201d544b5e5147080d81a4c5586be6bc3de
Yann Büchau <nobodyinperson@posteo.de>
2023-09-25 10:59:55 +0200

    Working shell.nix
    on NixOS at least... 🙄

--- shell.nix
+++ shell.nix
@@ -2,19 +2,30 @@
   # around NixOS 23.05
   "https://github.com/NixOS/nixpkgs/archive/9034b46dc4c7596a87ab837bb8a07ef2d887e8c7.tar.gz")
   { } }:
-let
-  pypkgs-build-requirements = {
-    lazy-loader = [ "flit" ];
-    contourpy = ["meson"]; # not enough, doesn't find meson in contourpy for some reason... 🙄
-  };
-  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
-    builtins.mapAttrs (package: build-requirements:
-      (builtins.getAttr package super).overridePythonAttrs (old: {
-        buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg:
-          if builtins.isString pkg then builtins.getAttr pkg super else pkg)
-          build-requirements);
-      })) pypkgs-build-requirements);
-in pkgs.poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
-  overrides = p2n-overrides;
-}
+(pkgs.buildFHSUserEnv rec {
+  name = "sdfCAD-env";
+  targetPkgs = pkgs:
+    (with pkgs; [
+      python3
+      poetry
+      zlib # for numpy
+    ]);
+  profile = ''
+    # create and fill virtual environment
+    poetry install --with=dev
+    # enter virtual environment
+    source "$(poetry env info --path)"/bin/activate
+    # Don't store notebook outputs
+    nbstripout --install
+    if ! python -c 'import tkinter' 2>/dev/null >/dev/null;then
+      # Fix tkinter import (shouldn't be necessary, but it is... Including tkinter in targetPkgs doesn't help)
+      export PYTHONPATH="${
+        toString pkgs.python3Packages.tkinter
+      }/lib/python3.10/site-packages/:$PYTHONPATH"
+    fi
+    export LD_LIBRARY_PATH="${
+      with pkgs; lib.makeLibraryPath [ xorg.libX11 xorg.libXi xorg.libXrender libGL ]
+    }:$LD_LIBRARY_PATH"
+    export PYTHONPATH="$(pwd):$PYTHONPATH"
+  '';
+}).env
